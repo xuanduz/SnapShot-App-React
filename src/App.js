54dfaa48f57779d100eb6b65816a9acc4df00aa0
xuanduz <xuanduc942001@gmail.com>
2021-12-26 13:02:14 +0700

    fix router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Nature from './components/pages/Nature'
 import SearchBar from './components/SearchBar';
 import ListPhoto from './components/ListPhoto';
 
-
 function App() {
   const [search, setSearch] = useState('')
 
@@ -22,23 +21,23 @@ function App() {
         <SearchBar handleClick={handleSearch} />
         <ul>
           <li>
-            <Link to='/' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link' >Home</Link>
+            <Link to='/SnapShot-App-React' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link' >Home</Link>
           </li>
           <li>
-            <Link to='/animal' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link'>Animal</Link>
+            <Link to='/SnapShot-App-React/animal' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link'>Animal</Link>
           </li>
           <li>
-            <Link to='./city' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link'>City</Link>
+            <Link to='/SnapShot-App-React/city' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link'>City</Link>
           </li>
           <li>
-            <Link to='/nature' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link'>Nature</Link>
+            <Link to='/SnapShot-App-React/nature' style={{ textDecoration: 'none', padding: '10px 15px' }} className='link'>Nature</Link>
           </li>
         </ul>
         <Routes>
-          <Route path='/' element={<ListPhoto searched={search} />}></Route>
-          <Route path='/animal' element={<Animal />}></Route>
-          <Route path='/city' element={<City />}></Route>
-          <Route path='/nature' element={<Nature />}></Route>
+          <Route path='/SnapShot-App-React/' element={<ListPhoto searched={search} />}></Route>
+          <Route path='/SnapShot-App-React/animal' element={<Animal />}></Route>
+          <Route path='/SnapShot-App-React/city' element={<City />}></Route>
+          <Route path='/SnapShot-App-React/nature' element={<Nature />}></Route>
         </Routes>
 
       </header>
